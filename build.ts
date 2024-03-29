0321d53398eb0817cf4f214220b8394e720cef1e
taisan <131413638+taisan11@users.noreply.github.com>
2024-03-29 14:57:21 +0900

    buildがねー...

--- build.ts
+++ build.ts
@@ -1,35 +1,25 @@
-import {build} from 'esbuild';
-// import {dependencies} from './package.json';
-async function Build(args: string[]) {
-  switch (args[2]) {
-    case 'cloudflare':
-      await Bun.build({
-        entrypoints: ['./src/index.tsx'],
-        outdir: './dist-cf',
-        target: 'node',
-        minify: true,
-      });
-      break;
-    // default:
-    //   build({
-    //     bundle: true,
-    //     entryPoints: ['./src/BBS.tsx', './src/TBS.tsx'],
-    //     logLevel: 'info',
-    //     sourcemap: true,
-    //     format: 'esm',
-    //     outdir: './dist',
-    //     target: ['ES6'],
-    //   })
-    //   break;
-    default:
-      Bun.build({
-        minify: true,
-        entrypoints: ['./src/BBS.tsx'],
-        // sourcemap: 'external',
-        // format: 'esm',
-        outdir: './dist',
-      })
-}
+async function main() {
+  console.log("Building...")
+  const BBS = Bun.file('./src/BBS.tsx')
+  const TBS = Bun.file('./src/TBS.tsx')
+  const KAS = Bun.file('./src/KAS.ts')
+  const subjectpaser = Bun.file('./src/subjectpaser.ts')
+  const kakiko = Bun.file('./src/kakiko.ts')
+  const datparser = Bun.file('./src/datparser.ts')
+  const newBBS = Bun.file("./dist/BBS.tsx");
+  const newTBS = Bun.file("./dist/TBS.tsx");
+  const newKAS = Bun.file("./dist/KAS.ts");
+  const newSubjectpaser = Bun.file("./dist/subjectpaser.ts");
+  const newKakiko = Bun.file("./dist/kakiko.ts");
+  const newDatparser = Bun.file("./dist/datparser.ts");
+  console.log("Writing...")
+    await Bun.write(newBBS, BBS);
+    await Bun.write(newTBS, TBS);
+    await Bun.write(newKAS, KAS);
+    await Bun.write(newSubjectpaser, subjectpaser);
+    await Bun.write(newKakiko, kakiko);
+    await Bun.write(newDatparser, datparser);
+    console.log("Build done!")
 }
 
-Build(Bun.argv)
+main()
